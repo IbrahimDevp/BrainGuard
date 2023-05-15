0886e04d45496a4af30b798474a9c7edc7522e82
ikema77 <brhoom.kh@gmail.com>
2023-05-15 15:05:29 +0300

    Defining CNN Model

--- BuildingModel.py
+++ BuildingModel.py
@@ -40,3 +40,13 @@ validation_generator = test_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='binary'
 )
+
+# Define the CNN model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
