5121be98e12a852aafceed9f48a9e81ebfec0f1c
ikema77 <brhoom.kh@gmail.com>
2023-05-15 15:04:27 +0300

    Importing required libraries

---
+++ BuildingModel.py
@@ -0,0 +1,7 @@
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.optimizers import Adam
+
+
