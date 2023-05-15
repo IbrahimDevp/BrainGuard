7230c5608e62363f264b5e17009398069066cc77
ikema77 <brhoom.kh@gmail.com>
2023-05-15 15:05:09 +0300

    Preprocessing the Images

--- BuildingModel.py
+++ BuildingModel.py
@@ -5,3 +5,38 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.optimizers import Adam
 
 
+# Define the paths to your training and testing data directories
+train_data_dir = 'dataset/TRAIN'
+test_data_dir = 'dataset/TEST'
+
+# Preprocessing steps
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255.0,
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    vertical_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+
+# Set the batch size and image dimensions
+batch_size = 16
+image_size = (256, 256)
+
+# Flow training images from the directory
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+# Flow validation images from the directory
+validation_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode='binary'
+)
