4d21e0dd2b6610e35cf013b8f295dbf1a18401d7
ikema77 <brhoom.kh@gmail.com>
2023-05-17 15:43:17 +0300

    Tuning parameters

--- BuildingModel.py
+++ BuildingModel.py
@@ -22,7 +22,7 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
 
 # Set the batch size and image dimensions
-batch_size = 16
+batch_size = 32
 image_size = (256, 256)
 
 # Flow training images from the directory
@@ -43,10 +43,16 @@ validation_generator = test_datagen.flow_from_directory(
 
 # Define the CNN model
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3), padding="same"))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding="same"))
+#model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding="same"))
+#model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
